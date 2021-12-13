792c2a715bb292fcd00a6049f2c28b62e162a7dd
Abhijit Roy <dalvath3700@gmail.com>
2021-12-13 14:19:23 +0530

    comments applied

--- tuts/enum/container_1.rs
+++ tuts/enum/container_1.rs
@@ -1,21 +1,30 @@
+/*
+	vector
+	- initialize
+	- push()
+	- pop()
+	- len()
+	- is_empty()
+*/
+
 fn main() {
 	let mut v = Vec::new();
 	assert!(v.is_empty());			// check the v is empty
 
+	// push & pop
 	v.push(1);
 	v.push(2);
 	v.push(32);
 	v.push(24);
 	v.pop();
 
-
+	// assertion
 	assert_eq!(v.len(), 3);
 	assert_eq!(v, [1, 2, 32]);
 	v[0] = 7;
 	assert_eq!(v, [7, 2, 32]);
 	assert_eq!(v[1], 2);
 	assert!(!v.is_empty());
-
 
 	// print the elements
 	for x in &v {
