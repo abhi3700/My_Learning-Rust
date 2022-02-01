5bbf99be8c13a997afdba4bc3cf62c52c464494e
Abhijit Roy <dalvath3700@gmail.com>
2022-02-01 13:29:28 +0530

    print modified

---
+++ tuts/print/print_1.rs
@@ -0,0 +1,8 @@
+/* 
+    Learn printing via positional argument
+    - Here, "Ramesh" is used twice, but defined once.
+*/
+
+fn main() {
+    println!("{0} is a good friend of {1}, but {1} hates {2}", "Abhijit", "Ramesh", "Sheetal");
+}
