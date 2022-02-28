6cd2787ed08dd343b9ea77fc4a4979a716fc0c81
Abhijit Roy <dalvath3700@gmail.com>
2022-02-28 23:41:07 +0530

    formatted the output, match pattern replaced the if-else

--- pro/calculator/src/main.rs
+++ pro/calculator/src/main.rs
@@ -3,7 +3,7 @@
 /// `unwrap()` is to capture the value if exist inside Some() used in nth function
 
 
-use std::env::{args, Args};         // `Args` is optional to use
+use std::{env::{args, Args}, result};         // `Args` is optional to use
 
 fn main() {
     let mut inputs = args();
@@ -12,44 +12,42 @@ fn main() {
     // M-1
     // let oper = inputs.nth(0).unwrap();
     // NOTE: to get the char out of string. As the string is an array of char. So, apply `char()` as iter & then extract the next or 1st element & then unwrap() if exist
-    // M-2
+    // M-2 [RECOMMENDED]
     let oper = inputs.nth(0).unwrap().chars().next().unwrap();
     let input_2 = inputs.nth(0).unwrap();
+
+    let num_1 = input_1.parse::<f64>().unwrap();
+    let num_2 = input_2.parse::<f64>().unwrap();
     // println!("{:?}", input_1);
     // println!("{:?}", oper);
     // println!("{:?}", input_2);
 
+    // ==================================================
     // print the result
     // M-1
     // println!("Result: {:?}", operate(oper.parse::<char>().unwrap(), input_1.parse::<f64>().unwrap(), input_2.parse::<f64>().unwrap()));
+    
     // M-2
-    println!("Result: {:?}", operate(oper, input_1.parse::<f64>().unwrap(), input_2.parse::<f64>().unwrap()));
+    let res = operate(oper, input_1.parse::<f64>().unwrap(), input_2.parse::<f64>().unwrap());
+    println!("{}", output(num_1, oper, num_2, res) );
     
 }
 
 // arithmetic function
 fn operate(oper: char, first_input: f64, second_input: f64) -> f64 {
-    // using `match` conditional
-    // let res = match oper {
-    //     '+' => input_1.parse::<u32>().unwrap() + input_2.parse::<u32>().unwrap(),
-    //     _ => 0
-    // };
-    
-    let mut res: f64 = 0.0;
-    if oper == '+' {
-        res = first_input + second_input; 
-    }
-    else if oper == '-' {
-        res = first_input - second_input; 
-    }
-    else if oper == '*' {
-        res = first_input * second_input; 
-    }
-    else if oper == '/' {
-        res = first_input / second_input; 
-    } else {
-        res = 0.0;
-    }
+    // using pattern matching
+    let res = match oper {
+        '+' => first_input + second_input,
+        '-' => first_input - second_input,
+        '*' => first_input * second_input,
+        '/' => first_input / second_input,
+        _ => 0.0
+    };
 
     res
+}
+
+// format method to output string
+fn output(first_input: f64, oper: char, second_input: f64, res: f64) -> String {
+    format!("{} {} {} = {}", first_input, oper, second_input, res)
 }
