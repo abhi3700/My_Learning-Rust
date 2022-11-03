0fb3b728ce5f850cb59ee2d25a1c2d5e76a3c76b
default <dalvath3700@gmail.com>
2022-11-03 21:53:43 +0530

    guess num game

--- tuts/rust_full/src/main.rs
+++ tuts/rust_full/src/main.rs
@@ -10,26 +10,18 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 #[allow(unused)]
 fn main() {
-    println!("Max u32: {}", std::u32::MAX);
-    println!("Max u64: {}", std::u64::MAX);
-    println!("Max u128: {}", std::u128::MAX);
-    println!("Max usize: {}", std::usize::MAX);
-    println!("Max f32: {}", std::f32::MAX);
+    let random_num = rand::thread_rng().gen_range(1..101);
+    println!("Guess the number!");
+    let mut guess_num = String::new();
+    io::stdin()
+        .read_line(&mut guess_num)
+        .expect("Failed to read line");
 
-    let is_true: bool = true;
-    println!("{}", is_true);
-
-    let my_grade = 'A';
-
-    let num_1: f32 = 1.111111111111111111;
-    println!("f32: {}", num_1 + 0.111111111111111111);
-    let num_2: f64 = 1.111111111111111111;
-    println!("f64: {}", num_2 + 0.111111111111111111);
-
-    let mut num_3: u32 = 5;
-    let num_4: u32 = 4;
-    println!("{} + {} = {}", num_3, num_4, num_3 + num_4);
-    println!("{} - {} = {}", num_3, num_4, num_3 - num_4);
-    println!("{} * {} = {}", num_3, num_4, num_3 * num_4);
-    println!("{} / {} = {}", num_3, num_4, num_3 / num_4);
+    let guess_num: u32 = guess_num.trim().parse().expect("Please type a number!");
+    if guess_num == random_num {
+        println!("You win!");
+    } else {
+        println!("You lose!");
+        println!("The secret number is: {}", random_num);
+    }
 }
