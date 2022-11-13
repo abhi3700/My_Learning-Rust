860a1ce5c365e5dd2c756ccb639b00949a97d9f0
default <dalvath3700@gmail.com>
2022-11-13 19:30:41 +0530

    match pattern using simple & cmp ordering

--- tuts/rust_full/src/main.rs
+++ tuts/rust_full/src/main.rs
@@ -17,6 +17,21 @@ fn main() {
         .expect("Please enter a valid age");
 
     let age: u8 = age.trim().parse().expect("Please enter a valid age");
-    let is_adult = if (age > 18) { true } else { false };
-    println!("Is adult: {}", is_adult);
+
+    //v1
+    // Here, the simple match pattern is used
+    let can_vote = match age {
+        1..=17 => false,
+        _ => true,
+    };
+    println!("Can vote: {}", can_vote);
+
+    // v2
+    // Here, the Ordering built-in is used
+    let voting_age = 18;
+    match age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't vote"),
+        Ordering::Greater => println!("Can vote"),
+        Ordering::Equal => println!("Can vote"),
+    }
 }
