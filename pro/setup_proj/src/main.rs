b75b3d0465d3772bf1c9cf9f93259d8b3671c922
default <dalvath3700@gmail.com>
2022-11-24 20:52:59 +0530

    setup_proj: v0.2.1 added pub to an struct inside another module in same file

--- pro/setup_proj/src/main.rs
+++ pro/setup_proj/src/main.rs
@@ -3,15 +3,19 @@ mod something {
         pub a: i32,
     }
 
-    struct B {
-        b: i32,
+    pub struct B {
+        pub b: i32,
     }
 }
 
-use crate::something::A;
+use crate::something::*;
 
 fn main() {
     let first = A { a: 67 };
 
     println!("{}", first.a);
+
+    let second = B { b: 69 };
+
+    println!("{}", second.b);
 }
