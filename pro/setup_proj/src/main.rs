0bd6f7cb731868fa6d773a6537a3183a07accfea
default <dalvath3700@gmail.com>
2022-11-24 20:50:11 +0530

    setup_proj: v0.2 added module in same file & imported

--- pro/setup_proj/src/main.rs
+++ pro/setup_proj/src/main.rs
@@ -1,10 +1,14 @@
-struct A {
-    pub a: i32,
+mod something {
+    pub struct A {
+        pub a: i32,
+    }
+
+    struct B {
+        b: i32,
+    }
 }
 
-struct B {
-    b: i32,
-}
+use crate::something::A;
 
 fn main() {
     let first = A { a: 67 };
