d9e70bd0ceb179ff52bef971c94767402498a2db
default <dalvath3700@gmail.com>
2023-01-06 15:30:03 +0530

    vector covered

--- tuts/rust_full/src/main.rs
+++ tuts/rust_full/src/main.rs
@@ -1,25 +1,7 @@
+fn sum_list(v: Vec<i32>) -> i32 {
+    v.iter().sum()
+}
+
 fn main() {
-    let v: Vec<i32> = Vec::new();
-    println!("{:?}", v);
-    let mut v2 = vec![1, 2, 3, 4];
-
-    v2.push(34);
-    v2.push(867);
-    println!("stored at: {:p}", &v2);
-
-    let second = &v2[1];
-    println!("{}", second);
-    println!("{}", &v2[1]);
-
-    match v2.get(1) {
-        Some(second) => println!("Found"),
-        None => panic!("Not found"),
-    }
-
-    for i in v2.iter_mut() {
-        println!("{}", i);
-        *i *= 2;
-    }
-
-    println!("v2 modified as: {:?}", v2);
+    println!("sum of vector is: {}", sum_list(vec![1, 2, 6, 8]));
 }
