5b8ce865c08fa6ed76882794723f59723b136e5c
default <dalvath3700@gmail.com>
2023-01-06 15:16:35 +0530

    vector added

--- tuts/rust_full/src/main.rs
+++ tuts/rust_full/src/main.rs
@@ -1,29 +1,25 @@
-enum Day {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
-}
-
-impl Day {
-    fn is_weekend(&self) -> bool {
-        match self {
-            Day::Saturday | Day::Sunday => {
-                println!("Weekend!");
-                true
-            }
-            _ => {
-                println!("Not a weekend");
-                false
-            }
-        }
-    }
-}
-
 fn main() {
-    let today = Day::Monday;
-    assert_eq!(today.is_weekend(), false);
+    let v: Vec<i32> = Vec::new();
+    println!("{:?}", v);
+    let mut v2 = vec![1, 2, 3, 4];
+
+    v2.push(34);
+    v2.push(867);
+    println!("stored at: {:p}", &v2);
+
+    let second = &v2[1];
+    println!("{}", second);
+    println!("{}", &v2[1]);
+
+    match v2.get(1) {
+        Some(second) => println!("Found"),
+        None => panic!("Not found"),
+    }
+
+    for i in v2.iter_mut() {
+        println!("{}", i);
+        *i *= 2;
+    }
+
+    println!("v2 modified as: {:?}", v2);
 }
