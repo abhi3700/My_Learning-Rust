4d0d71125635c9c749488cb59187af938760dec6
abhi3700 <dalvath3700@gmail.com>
2023-11-29 17:58:40 +0530

    Structured properly

---
+++ tuts/src/main.rs
@@ -0,0 +1,10 @@
+//! Rust By Practice
+//! ================
+//! https://practice.rs/compound-types/enum.html-1
+
+#[path = "../topics/collection/arr_1.rs"]
+mod arr_1;
+
+fn main() {
+    arr_1::main();
+}
---
+++ tuts/topics/collection/arr_1.rs
@@ -0,0 +1,21 @@
+/*
+    Use array
+    - arrays are stack allocated
+    - define array with elements
+    - print elements via for-loop
+*/
+
+pub fn main() {
+    let x: [i32; 5] = [41, 53, 45, 37, 89];
+
+    // M-1
+    for i in 0..x.len() {
+        println!("{}", x[i]);
+    }
+
+    // M-2
+    println!("{:?}", x);
+
+    // M-3
+    println!("{:#?}", x);
+}
