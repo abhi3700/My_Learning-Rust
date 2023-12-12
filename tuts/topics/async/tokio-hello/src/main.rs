8821bbb12f8ec4b55cceb60e8270921b2d0ef3d0
abhi3700 <dalvath3700@gmail.com>
2023-12-12 14:13:54 +0530

    Refactor Redis Operations for Async Concurrency in Rust
    - Replaced sequential execution of Redis set/get operations with concurrent tasks.
    - Introduced  to enable parallel processing of Redis commands, enhancing throughput and performance.
    - Utilized  for shared client ownership across async tasks, ensuring thread safety and efficient resource management.
    - Implemented error handling for nested  types in concurrent async contexts.
    - The refactor aligns with async Rust best practices, leveraging Tokio's full capabilities for IO-bound tasks.
    - This change is beneficial for scenarios where independent Redis operations can be executed concurrently for better efficiency.
    - Note: Increased complexity due to concurrent operations necessitates careful handling of potential race conditions and deadlocks.

--- tuts/topics/async/tokio-hello/src/main.rs
+++ tuts/topics/async/tokio-hello/src/main.rs
@@ -1,3 +1,5 @@
+//! **I/O bound Async**
+//! ===================
 //! Run a mini redis server to set value to a key named "hello"
 //! ```sh
 //! # Install the mini-redis-server
@@ -16,44 +18,66 @@
 //! ```sh
 //! Error: Os { code: 61, kind: ConnectionRefused, message: "Connection refused" }
 //! ```
+//!
+//! Commits (in increasing order of efficient code):
+//! 1. [x] Simple connect to Redis server & set/get {hello: world}
+//! 2. [x] Sharing resource via Arc & Mutex: https://github.com/abhi3700/My_Learning-Rust/blob/751f6bd6ec88bcb75fa7bcc26771b39df4f3b840/tuts/topics/async/tokio-hello/src/main.rs
+//!     The different scopes in this code are used to ensure that the lock on the `Mutex` is released as soon as it's no longer needed.
+//!     In Rust, a `MutexGuard` (the type returned by `lock().await`) is automatically unlocked when it goes out of scope. By using
+//!     separate scopes for each operation, the code ensures that the lock is released immediately after each operation completes,
+//!     allowing other tasks to acquire the lock if needed.
+//!     If all operations were performed in the same scope, the lock would be held for the entire duration of the scope, potentially
+//!     preventing other tasks from acquiring the lock even when it's not actively being used. This could lead to unnecessary contention
+//!     and reduced concurrency.
+//!     So, the use of different scopes in this code is a way to manage the lifetime of the `MutexGuard` and ensure that locks are held
+//!     for the shortest time possible.
+//!     Use `RwLock` instead of Mutex. But not recommended in this as it won't add any efficiency.
+//!     If multiple threads are frequently trying to acquire the same lock, it can lead to performance issues. Consider using finer-grained
+//!     locks, or other synchronization primitives like RwLock which allows multiple readers or one writer at a time.
+//! 3. [ ] Concurrent approach of handling I/O tasks
 
 use mini_redis::{client, Result};
 use std::sync::Arc;
 use tokio::sync::Mutex;
-
-async fn say_world() {
-    println!("world");
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let client = client::connect("127.0.0.1:6379").await?;
     let client = Arc::new(Mutex::new(client));
 
-    // setting the value to "world"
-    {
-        let mut locked_client = client.lock().await;
-        locked_client.set("hello", "world".into()).await?;
-    }
-    // getting the value
-    {
-        let mut locked_client = client.lock().await;
-        let res = locked_client.get("hello").await?;
-        println!("{:?}", res);
-    }
+    // Concurrently setting the values to "world" and "world2"
+    let client_clone = Arc::clone(&client);
+    let set_hello = tokio::spawn(async move {
+        let mut locked_client = client_clone.lock().await;
+        locked_client.set("hello", "world".into()).await
+    });
 
-    // setting the value to "world2"
-    {
-        let mut locked_client = client.lock().await;
-        locked_client.set("hello2", "world2".into()).await?;
-    }
+    let client_clone = Arc::clone(&client);
+    let set_hello2 = tokio::spawn(async move {
+        let mut locked_client = client_clone.lock().await;
+        locked_client.set("hello2", "world2".into()).await
+    });
 
-    // getting the value
-    {
-        let mut locked_client = client.lock().await;
-        let res = locked_client.get("hello2").await?;
-        println!("{:?}", res);
-    }
+    // Wait for the set operations to complete
+    set_hello.await??;
+    set_hello2.await??;
+
+    // Concurrently getting the values of "hello" and "hello2"
+    let client_clone = Arc::clone(&client);
+    let get_hello = tokio::spawn(async move {
+        let mut locked_client = client_clone.lock().await;
+        locked_client.get("hello").await
+    });
+
+    let client_clone = Arc::clone(&client);
+    let get_hello2 = tokio::spawn(async move {
+        let mut locked_client = client_clone.lock().await;
+        locked_client.get("hello2").await
+    });
+
+    // Wait for the get operations to complete and print results
+    println!("{:?}", get_hello.await??);
+    println!("{:?}", get_hello2.await??);
 
     Ok(())
 }
